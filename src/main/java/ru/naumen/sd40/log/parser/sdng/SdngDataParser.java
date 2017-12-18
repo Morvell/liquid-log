d57eff6a5b598a7005122f2b80f06390dace65d3
morvell <zaxarov2702@yandex.ru>
2017-12-18 13:58:38 +0500

    simple sdng

--- src/main/java/ru/naumen/sd40/log/parser/sdng/SdngDataParser.java
+++ src/main/java/ru/naumen/sd40/log/parser/sdng/SdngDataParser.java
@@ -7,12 +7,20 @@ import ru.naumen.sd40.log.parser.IData;
 @Component
 public class SdngDataParser implements IDataParser {
 
+    private final ActionDoneParser actionDoneParser;
+    private final ErrorParser errorParser;
+
+    public SdngDataParser() {
+        actionDoneParser = new ActionDoneParser();
+        errorParser = new ErrorParser();
+    }
+
     @Override
     public void parseLine(IData data, String line) {
 
         ISdngData sdngData = (ISdngData) data;
-        new ActionDoneParser().parseLine(sdngData.getActionsDone(),line);
-        new ErrorParser().parseLine(sdngData.getErrors(),line);
+        actionDoneParser.parseLine(sdngData.getActionsDone(),line);
+        errorParser.parseLine(sdngData.getErrors(),line);
     }
 
 }
